cdb97de99b374805a0d7603af3ba5b3b529fa29d
xuyc <xmcy0011@sina.com>
2022-07-28 08:23:58 +0800

    add debug log

--- hqr/app/src/main/java/cn/nr/hqrfid/ydmhapi/YDMHEntryActivity.java
+++ hqr/app/src/main/java/cn/nr/hqrfid/ydmhapi/YDMHEntryActivity.java
@@ -90,6 +90,9 @@ public class YDMHEntryActivity extends Activity implements IYDMHAPIEventHandler 
 
     @Override
     public void onResp(SendAuth.Resp resp) {
+        Toast.makeText(YDMHEntryActivity.this, "onResp, authCode:" + resp.authCode + ",state:" + resp.mState,
+                Toast.LENGTH_SHORT).show();
+
         switch (resp.errCode) {
             case SendAuth.ErrCode.ERR_USER_CANCEL:
                 Log.d(TAG, "取消授权");
